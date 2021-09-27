e7926f038bb30aef2abb31b9df249b3feafc909b
Christoffel Angga <73043976+CloverJoy@users.noreply.github.com>
2021-09-27 16:09:27 -0700

    Update BSTconstruction.js

--- BSTconstruction.js
+++ BSTconstruction.js
@@ -1,3 +1,7 @@
+// Do not edit the class below except for
+// the insert, contains, and remove methods.
+// Feel free to add new properties and methods
+// to the class.
 class BST {
   constructor(value) {
     this.value = value;
@@ -6,24 +10,23 @@ class BST {
   }
 
   insert(value) {
-    // Write your code here.
-    // Do not edit the return statement of this method.
-		if (value > this.value) {
+		if (value < this.value) {
 			if (this.left === null) {
-				this.left = new BST(value);
+				this.left = new BST(value)
 			} else {
-				this.left.insert(value)
+				this.left.insert(value);
 			}
 		} else {
-				if (this.right === null) {
-					this.right = new BST(value);
-				} else {
-					this.right.insert(value);
-				}
+			if (this.right === null) {
+				this.right = new BST(value)
+			} else {
+				this.right.insert(value);
 			}
-		return this;
 		}
-  
+    // Write your ////code here.
+    // Do not edit the return statement of this method.
+    return this
+  }
 
   contains(value) {
     // Write your code here.
@@ -42,16 +45,52 @@ class BST {
 		} else {
 			return true;
 		}
-		
   }
 
-  remove(value) {
+  remove(value, parent = null) {
+		if (value < this.value) {
+			if (this.left !== null) {
+				this.left.remove(value,this);
+			} 
+	} else if (value > this.value) {
+			this.right.remove(value,this);
+		} else {
+		if (this.left !== null && this.right !== null) {
+			this.value = this.right.getMinValue();
+			this.right.remove(this.value, this);
+		} else if (parent === null) {
+			if (this.left !== null) {
+				this.value = this.left.value;
+				this.right = this.left.right;
+				this.left = this.left.left;
+			} else if (this.right !== null) {
+				this.value = this.right.value;
+				this.left = this.right.left;
+				this.right = this.right.right
+			} else {
+				
+			}
+		} else if (parent.left === this) {
+			parent.left = this.left !== null ? this.left : this.right;
+		} else if (parent.right === this) {
+			parent.right = this.left !== null ? this.left: this.right;
+		}
+	}
     // Write your code here.
     // Do not edit the return statement of this method.
-		
     return this;
   }
+	
+	getMinValue() {
+	if (this.left === null) {
+		return this.value;
+	} else {
+		return this.left.getMinValue()
+	}
 }
+}
+
+
 
 // Do not edit the line below.
 exports.BST = BST;
